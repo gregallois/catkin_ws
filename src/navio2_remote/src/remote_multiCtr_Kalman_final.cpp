2ce7a4b5721c9f88f0352e8114ba8412b56d95b5
Gallois <gallois@tsf-428-wpa-5-125.epfl.ch>
2017-03-17 15:09:44 +0100

    d

--- src/navio2_remote/src/remote_multiCtr_Kalman_final.cpp
+++ src/navio2_remote/src/remote_multiCtr_Kalman_final.cpp
@@ -647,7 +647,8 @@ bool checkOutlier(float covariance[3][3], float mean[3][1], float point[3][1])
 			{
 
 				if (first_gps == 0) //initialize the first value of GPS for position to Kalman (assuming we leave from the ground station)
-				{	
+				{
+                    prinf("OM DEVVRQIT PASSSSSSSS\n");
 					mu_kalman[0][0] = 0;
 					mu_kalman[1][0] = 0;
 					first_gps == 1;
@@ -667,7 +668,9 @@ bool checkOutlier(float covariance[3][3], float mean[3][1], float point[3][1])
                 //State estimation
                 mu_kk_1[2][0] = Kalman_evalYaw(mu_kalman[2][0], currentYaw, oldYaw);
 				mu_kk_1[0][0] = Kalman_evalX(mu_kalman[0][0], currentSpeed, mu_kk_1[2][0], (float)dT);
-				mu_kk_1[1][0] = Kalman_evalY(mu_kalman[1][0], currentSpeed, mu_kk_1[2][0], (float)dT);
+				mu_kk_1[0][0 = Kalman_evalY(mu_kalman[1][0], currentSpeed, mu_kk_1[2][0], (float)dT);
+                
+                prinf("mu_kk_1): %f, &f\n", mu_kk_1[0][0], mu_kk_1[1][0]);
                 oldYaw = currentYaw;
                 
                 
@@ -702,6 +705,7 @@ bool checkOutlier(float covariance[3][3], float mean[3][1], float point[3][1])
 				else{
 					equal31(mu_kk_1,mu_kalman);
 					equal33(P_kk_1,Kalman_P);
+                    prinf("mu_kalman): %f, &f\n", mu_kalman[0][0], mu_kalman[1][0]);
 				}
 				
 			}
